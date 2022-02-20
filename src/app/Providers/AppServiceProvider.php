46e2c0396bdb1aa7c814eba4c8dabebe366bf129
danshuitaihejie <474182370@qq.com>
2022-02-21 01:03:00 +0800

    add laravel/jetstream for login. use sqlite db.
    新增登录模块，并在开发期间启用sqlite方便运行，
    请自行修改 "/src/.env" 文件中的"DB_DATABASE"参数所指向的sqlite数据库文件，
    然后再执行 php artisan migrate 初始化数据库。

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+		/**
+		解决类似问题：Illuminate\Database\QueryException
+  SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; 
+  max key length is 1000 bytes (SQL: alter table `users` add unique `users_email_unique`(`email`))
+  参见：https://laravel.com/docs/master/migrations#creating-indexes
+		 */
+		Schema::defaultStringLength(191);
+      
     }
 }
