b014171ee33549e1bf75432a2378acdeba7cd2ae
Yanhui Li <whimet@gmail.com>
2022-03-12 21:26:26 +1100

    Make show page simple

--- src/resources/views/diagrams/show.blade.php
+++ src/resources/views/diagrams/show.blade.php
@@ -1,49 +1,47 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Diagram') }}
-        </h2>
-        
+<!DOCTYPE html>
+<html lang="">
+<header>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="icon" href="/favicon.ico">
+    <title>{{ $diagram->name }}</title>
+
     <link href="/sequence-viewer/css/app.601d2ada.css" rel="preload" as="style">
     <link href="/sequence-viewer/css/chunk-vendors.d27486a6.css" rel="preload" as="style">
     <link href="/sequence-viewer/js/app.96bc52c6.js" rel="preload" as="script">
     <link href="/sequence-viewer/js/chunk-vendors.a42931a3.js" rel="preload" as="script">
     <link href="/sequence-viewer/css/chunk-vendors.d27486a6.css" rel="stylesheet">
     <link href="/sequence-viewer/css/app.601d2ada.css" rel="stylesheet">
-        
-    </x-slot>
-    
-    <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            
-          <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Name:</strong>
-                    <span>{{ $diagram->name }}</span>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Description</strong>
-                    <span>{{ $diagram->description }}</span>
-                </div>
-            </div>
-          </div>
-          <div class="row">
+</header>
+
+<body>
+<div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        <div class="row">
             <a class="btn btn-primary" href="/diagrams" title="Go back"> Go back </a>
-          </div>
+        </div>
+        <div class="row">
+        
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description</strong>
+                <span>{{ $diagram->description }}</span>
+            </div>
+        </div>
         </div>
     </div>
+</div>
 
-    <div id="app"></div>
+<div id="app"></div>
 
-    <script src="/sequence-viewer/js/chunk-vendors.a42931a3.js"></script>
-    <script src="/sequence-viewer/js/app.96bc52c6.js"></script>
-    <script>
-        let app = document.getElementById('app');
-        if (app.__vue__) {
-            app.__vue__.$store.commit('code',`{{ $diagram->content }}`)
-        }
-    </script>
-</x-app-layout>
+<script src="/sequence-viewer/js/chunk-vendors.a42931a3.js"></script>
+<script src="/sequence-viewer/js/app.96bc52c6.js"></script>
+<script>
+    let app = document.getElementById('app');
+    if (app.__vue__) {
+        app.__vue__.$store.commit('code',`{{ $diagram->content }}`)
+    }
+</script>
+</body>
+</html>
