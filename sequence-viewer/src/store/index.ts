c5eb0806bce63d5d5540903b270d44b62d5b814b
Yanhui Li <whimet@gmail.com>
2022-04-02 13:16:42 +1100

    Hide diagram info UI on edit mode

--- sequence-viewer/src/store/index.ts
+++ sequence-viewer/src/store/index.ts
@@ -4,30 +4,45 @@ import { VueSequence } from 'vue-sequence';
 
 Vue.use(Vuex)
 
-const baseStore = VueSequence.Store()
-
-export default new Vuex.Store({
+const baseStore = VueSequence.Store();
+const store = {
   ...baseStore,
   state: {
     ...baseStore.state,
-    mode: 'view'
+    mode: 'view',
+    modeListeners: []
   },
   mutations: {
     ...baseStore.mutations,
     mode(state: any, payload: string) {
       state.mode = payload;
-    }
+    },
+    modeListeners(state: any, payload: Array<any>) {
+      state.modeListeners = payload;
+    },
   },
   actions: {
     ...baseStore.actions,
-    setEditMode({commit}: any) {
+    setEditMode({commit, state}: any) {
       commit('mode', 'edit');
+      state.modeListeners.filter((l: any) => l.mode === 'edit').forEach((l: any) => l.payload());
     },
-    setViewMode({commit}: any) {
+    setViewMode({commit, state}: any) {
       commit('mode', 'view');
+      state.modeListeners.filter((l: any) => l.mode === 'view').forEach((l: any) => l.payload());
     },
     toggleMode({commit, state}: any) {
-      commit('mode', state.mode === 'view' ? 'edit' : 'view');
+      state.mode === 'view'
+        ? store.actions.setEditMode({commit, state})
+        : store.actions.setViewMode({commit, state});
+    },
+    registerEditModeListener({commit, state}: any, payload: any) {
+      state.modeListeners.push({mode: 'edit', payload});
+      commit('modeListeners', state.modeListeners);
+    },
+    registerViewModeListener({commit, state}: any, payload: any) {
+      state.modeListeners.push({mode: 'view', payload});
+      commit('modeListeners', state.modeListeners);
     },
   },
   getters: {
@@ -36,4 +51,6 @@ export default new Vuex.Store({
       return state.mode === 'edit';
     }
   }
-})
+};
+
+export default new Vuex.Store(store)
