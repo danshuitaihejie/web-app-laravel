64cc39faecba668e995174f1af025ce428d9e836
R0uter <lomyr.jo@gmail.com>
2022-04-02 18:03:06 +1000

    user dashboard layout done

---
+++ resources/views/components/dashboard-my-card.blade.php
@@ -0,0 +1,13 @@
+<div class="flex flex-col rounded-lg shadow-lg overflow-hidden min-w-fit bg-white">
+    <div class="flex-shrink-0">
+        <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">
+    </div>
+    <div class="flex-1  p-6 flex flex-col justify-between flex-wrap max-w-fit">
+        <div class="flex-1">
+            <a href="#" class="block mt-2">
+                <p class="text-xl font-semibold text-gray-900">My lovely diagram!</p>
+                <p class="mt-3 text-sm text-gray-500">Last edit: 2022-04-02</p>
+            </a>
+        </div>
+    </div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,33 +1,53 @@
 <x-app-layout>
 
-    <div class="flex justify-evenly py-5">
-        <x-jet-button>+ New Diagram</x-jet-button>
-        <x-jet-button>+ Import Diagram</x-jet-button>
-        <x-jet-button>+ Import Diagram12</x-jet-button>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-4 gap-10 h-20 my-6 ">
+            <x-jet-button class="items-center">+ New Diagram</x-jet-button>
+            <x-jet-button class="items-center">+ Import a Diagram</x-jet-button>
+            <x-jet-button class="items-center">+ Invite People!</x-jet-button>
+        </div>
+
     </div>
 
-    <div class="!bg-white">
-        <div class="container py-1 mx-auto">
-            <div class="container py-1 mx-auto">
-                <x-jet-label>Explore:</x-jet-label>
+    <div class="bg-white">
+        <div class="container mx-auto">
+            <div class="container pt-5 mx-auto ">
+                <x-jet-label class="text-lg">Explore:</x-jet-label>
             </div>
-            <div class="flex justify-start py-5">
-                <x-jet-button>+ New Diagram</x-jet-button>
-                <x-jet-button>+ Import Diagram</x-jet-button>
-                <x-jet-button>+ Import Diagram12</x-jet-button>
+            <div class="flex flex-row flex-nowrap overflow-x-auto  flex-start gap-8 pt-5 pb-10">
+
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
+                <x-dashboard-explore-card></x-dashboard-explore-card>
             </div>
         </div>
     </div>
 
     <div class="">
         <div class="container py-1 mx-auto">
-            <div class="container py-1 mx-auto">
-                <x-jet-label>My Diagrams:</x-jet-label>
+            <div class="container py-1 mx-auto pt-5">
+                <x-jet-label class="text-lg">My Diagrams:</x-jet-label>
             </div>
-            <div class="flex justify-start py-5">
-                <x-jet-button>+ New Diagram</x-jet-button>
-                <x-jet-button>+ Import Diagram</x-jet-button>
-                <x-jet-button>+ Import Diagram12</x-jet-button>
+            <div class="grid grid-cols-4 gap-8 py-5">
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
+                <x-dashboard-my-card></x-dashboard-my-card>
             </div>
         </div>
     </div>
