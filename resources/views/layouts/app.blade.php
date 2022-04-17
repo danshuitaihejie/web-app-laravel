1cd3d4b1b6a156f284954c7c5575740f4d7e6d76
Yanhui Li <whimet@gmail.com>
2022-04-17 15:01:09 +1000

    Use local google fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,8 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <!-- Downloaded from https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap -->
+        <link rel="stylesheet" href="/css/nunito-fonts.css">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
