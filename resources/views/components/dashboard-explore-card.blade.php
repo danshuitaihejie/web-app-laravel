b56521853819e0103891bbe3b4f1ba4570239a7b
Yanhui Li <whimet@gmail.com>
2022-04-18 13:43:40 +1000

    Unify image size in dashboard

--- resources/views/components/dashboard-explore-card.blade.php
+++ resources/views/components/dashboard-explore-card.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col rounded-lg shadow-lg overflow-hidden min-w-fit">
     <div class="flex-shrink-0">
-        <img class="h-48 w-full object-cover" src="{{ Illuminate\Support\Facades\Storage::url($diagram->image) }}" alt="">
+        <img class="dashboard-card object-cover" src="{{ Illuminate\Support\Facades\Storage::url($diagram->image) }}" alt="">
     </div>
     <div class="flex-1 bg-white p-6 flex flex-col justify-between flex-wrap max-w-fit">
         <div class="flex-1">
