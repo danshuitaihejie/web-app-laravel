b674ed92d5520b85bfb9d58361226feff89fbd84
MrCoder <eagle.xiao@gmail.com>
2022-04-23 18:44:04 +1000

    Added color to theme selectors.

--- resources/views/diagrams/show.blade.php
+++ resources/views/diagrams/show.blade.php
@@ -245,25 +245,25 @@
               <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none ring-gray-400">
                 <input type="radio" name="color-choice" value="White" class="sr-only" aria-labelledby="color-choice-0-label" onclick="setTheme()">
                 <p id="color-choice-0-label" class="sr-only">White</p>
-                <span aria-hidden="true" class="h-8 w-8 bg-white border border-black border-opacity-10 rounded-full"></span>
+                <span aria-hidden="true" class="h-8 w-8 bg-white border border-black rounded-full"></span>
               </label>
 
-              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none ring-gray-400">
+              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none border-black ">
                 <input type="radio" name="color-choice" value="BlackWhite" class="sr-only" aria-labelledby="color-choice-1-label" onclick="setTheme('theme-black-white')">
                 <p id="color-choice-1-label" class="sr-only">Black White</p>
-                <span aria-hidden="true" class="h-8 w-8 bg-gray-900 border border-black border-opacity-10 rounded-full"></span>
+                <span aria-hidden="true" class="h-8 w-8 bg-white border border-black rounded-full shadow-[2px_2px_0px_rgba(0,0,0,1)]"></span>
               </label>
 
-              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none ring-gray-400">
+              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none bg-blue-900">
                 <input type="radio" name="color-choice" value="Blue" class="sr-only" aria-labelledby="color-choice-1-label" onclick="setTheme('theme-blue')">
                 <p id="color-choice-1-label" class="sr-only">Blue</p>
-                <span aria-hidden="true" class="h-8 w-8 bg-gray-900 border border-black border-opacity-10 rounded-full"></span>
+                <span aria-hidden="true" class="h-8 w-8 bg-blue-100 border border-black border-opacity-10 rounded-full"></span>
               </label>
 
-              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none ring-gray-400">
+              <label class="-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none bg-rose-800">
                 <input type="radio" name="color-choice" value="StarUML" class="sr-only" aria-labelledby="color-choice-1-label" onclick="setTheme('theme-star-uml')">
                 <p id="color-choice-1-label" class="sr-only">Star UML</p>
-                <span aria-hidden="true" class="h-8 w-8 bg-gray-900 border border-black border-opacity-10 rounded-full"></span>
+                <span aria-hidden="true" class="h-8 w-8 bg-amber-200 border border-black border-opacity-10 rounded-full"></span>
               </label>
             </div>
           </fieldset>
